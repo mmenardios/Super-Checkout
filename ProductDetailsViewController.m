f46ccfbdb3b39be916154a15e021b71931324c1e
mmenardios <michel.menard@univ-lr.fr>
2012-03-28 19:28:53 +0200

    Michel

--- ProductDetailsViewController.m
+++ ProductDetailsViewController.m
@@ -4,7 +4,7 @@
 //
 //  Created by Brandon Alexander on 3/8/11.
 //  Copyright 2011 While This, Inc. All rights reserved.
-//
+//   Michek
 
 #import "ProductDetailsViewController.h"
 #import "QuantityCell.h"
